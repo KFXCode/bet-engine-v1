e363e4be42a1807e9c738b2802088fe28d2ab304
Cover1picks <kvf.business22@gmail.com>
2026-07-30 08:58:17 -0400

    Update run_daily.py

--- run_daily.py
+++ run_daily.py
@@ -17,6 +17,7 @@ from data.schedule_provider_nfl import get_todays_nfl_games
 from data.schedule_provider_ncaaf import get_todays_ncaaf_games
 from data.schedule_provider_ncaab import get_todays_ncaab_games
 from data.schedule_provider_nhl import get_todays_nhl_games
+from data.schedule_provider_nba import get_todays_nba_games
 from data.odds_providers import get_odds_provider
 from data.public_betting_provider import get_public_betting_provider
 from data.stats_provider import get_stats_provider
@@ -79,6 +80,8 @@ def _fetch_schedule(sport, date_str):
         return get_todays_ncaab_games(date_str)
     if sport == "NHL":
         return get_todays_nhl_games(date_str)
+    if sport == "NBA":
+        return get_todays_nba_games(date_str)
     logger.warning("No schedule provider wired for enabled sport %s -- skipping.", sport)
     return []
 
